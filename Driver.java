02f6b74df45a0df40b48232870e34f4ca0c3e4da
JackHigg <70903494+JackHigg@users.noreply.github.com>
2021-09-28 10:58:28 +0100

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Driver {
+
+    static Scanner kboard= new Scanner(System.in);
 
     public static void main(String[] args){
 
-        TwelveHourClock time=new TwelveHourClock();
 
-        time.displayTime12();
-        time.displayTime();
+        TwelveHourClock time=new TwelveHourClock();    //declares new object time
+
+        int choice;
+
+        System.out.println("What format would you like to see the time 1:24 hour format 2:12 hour format");
+        choice =kboard.nextInt();
+
+        if (choice==1){
+            time.displayTime();
+        }
+        else
+        {
+            time.displayTime12();
+        }
 
 
 
